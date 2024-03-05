a31a6aeae9e345fcc80a1d26b9854c6e191edfa8
YXXNHX <50559373+yxxnhx@users.noreply.github.com>
2024-03-05 10:45:03 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+const solution = (price) => {
+    if(price >= 500000) {
+        return Math.floor(price * 0.8)
+    } else if(price >= 300000) {
+        return Math.floor(price * 0.9)
+    } else if (price >= 100000) {
+        return Math.floor(price * 0.95)
+    } else {
+        return Math.floor(price)
+    }
+    
+    return price;
+}
