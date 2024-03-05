84525f817346b4389f8a954314304331a633fcfc
YXXNHX <50559373+yxxnhx@users.noreply.github.com>
2024-03-05 13:55:01 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,10 @@
+function solution(num) {
+    let count = 0;
+    
+    while(num !== 1 && count !== 500) {
+        num % 2 === 0 ? num = num / 2 : num = num * 3 + 1;
+        count++;
+    }
+    
+    return num === 1 ? count : -1;
+}
