6f7bf0e27d6b6055d368cf0b6afc575dc030d258
YXXNHX <50559373+yxxnhx@users.noreply.github.com>
2024-03-07 15:11:55 +0900

    [level 1] Title: 삼총사, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,12 @@
+function solution(number) {
+    let answer = 0;
+    for(i=0; i<number.length; i++) {
+        for(j= i+1; j<number.length; j++) {
+            for(k= j+1; k<number.length; k++) {
+                number[i] !== number[j] !== number[k] 
+                    ? (number[i] + number[j] + number[k] === 0 ? answer++ : '') : ''
+            }
+        }
+    }
+    return answer;
+}
