6969d938d7e3aafd8073183031d51125b615fe26
YXXNHX <50559373+yxxnhx@users.noreply.github.com>
2026-03-19 01:33:59 +0900

    [level 0] Title: 공배수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -0,0 +1,11 @@
+function solution(number, n, m) {
+    var answer = 0;
+    if(number % n === 0 && number % m === 0) {
+        answer = 1
+    } 
+    
+        if(number % n !== 0 && number % m !== 0) {
+        answer = 0
+    } 
+    return answer;
+}
